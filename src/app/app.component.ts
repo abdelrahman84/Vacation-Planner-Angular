c712f844be511b0ea08132f8c630b0759d2f8bb5
abdelrahman84 <abdelrahman.farag84@gmail.com>
2019-03-08 08:49:20 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   title = 'VacationPlannerAngular';
-  vacationBalance=21;
+  
+    // get the integer value in case it was stored as a string
+    
+    
   annualVacation=15;
   CasualBalance=6;
+  vacationBalance=this.annualVacation+this.CasualBalance;
   public show:boolean = false;
   public buttonName:any = 'Show';
+  public selectedVacationType;
+
+  options = [
+    { name: "Annual", value: 1 },
+    { name: "Casual", value: 2 }
+  ]
 
   ngOnInit () {  }
 
   toggle() {
     this.show = !this.show;
+   }
 
-   
- 
-}
+  decrement(){
+    if (this.selectedVacationType=="Annual"){
+      this.annualVacation--;
+      this.vacationBalance=this.annualVacation+this.CasualBalance;
+    }
+    else if (this.selectedVacationType=="Casual"){
+      this.CasualBalance--;
+      this.vacationBalance=this.annualVacation+this.CasualBalance;
+    }
+
+  }
+
 }
