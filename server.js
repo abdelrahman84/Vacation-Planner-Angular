5f6e2e33c2755f9f234f2516738fa9afef8f842e
abdelrahman84 <abdelrahman.farag84@gmail.com>
2019-05-11 15:06:04 +0200

    fixing bugs

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/VacationPlannerAngular'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/VacationPlannerAngular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
